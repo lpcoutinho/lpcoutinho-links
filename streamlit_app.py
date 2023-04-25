4fb51eb4ae9c44184c956035deb59e1cc1c62d32
Luiz Paulo Coutinho <coutinholps@gmail.com>
2023-04-25 01:53:47 -0300

    another photo

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ load_css()
 # st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
 
 col1, col2, col3 = st.columns(3)
-col2.image(Image.open('me.png'))
+col2.image(Image.open('me.jpeg'))
 
 st.header('Luiz Paulo Coutinho, Developer')
 
